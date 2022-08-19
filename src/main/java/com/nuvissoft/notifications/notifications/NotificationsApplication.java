9bee8f4c8d8c4f0fc58811988d630fb3074a09bc
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2022-08-18 20:33:32 -0600

    removing comments

--- src/main/java/com/nuvissoft/notifications/notifications/NotificationsApplication.java
+++ src/main/java/com/nuvissoft/notifications/notifications/NotificationsApplication.java
@@ -4,65 +4,67 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nuvissoft.notifications.notifications.Services.CreditServices;
-// import com.nuvissoft.notifications.notifications.Services.WhatsAppSender;
 import com.nuvissoft.notifications.notifications.Services.WhatsAppSender;
 import com.twilio.type.PhoneNumber;
 
 import Utilities.Environment;
-// import Data.Fake.FakeDataExamples;
 
 @SpringBootApplication
 public class NotificationsApplication {
 
 	public static void main(String[] args) throws InterruptedException {
+	
 		SpringApplication.run(NotificationsApplication.class, args);
 		WhatsAppSender whatsApp = new WhatsAppSender();
 		demoWhatsAppMessageWithExcel(whatsApp);
+
+		
 	}
 
 	static void demoWhatsAppMessageWithExcel(WhatsAppSender whatsApp) {
+		
 		CreditServices service = new CreditServices();
 		String messageTemplate = Environment.env.get("TWILIO_CUSTOM_MESSAGE");
 		String paymentReportMessage = Environment.env.get("PAYMENT_REPORT_MESSAGE");
 		var notificationsSended = service.getAllCreditsWithLate();
 
-		// notificationsSended
-		// 		.forEach(e -> {
+		notificationsSended
+				.forEach(e -> {
 
-		// 			String finalMessage = messageTemplate
-		// 					.replace(
-		// 							"NOMBRE",
-		// 							e.getFirstName())
-		// 					.replace(
-		// 							"MONTO",
-		// 							String.valueOf(
-		// 									e.getAmount()))
-		// 					.replace(
-		// 							"OBSERVACION",
-		// 							"Este mensaje duró en llegar: " + (e.getTerm()) + " Sec");
+					String finalMessage = messageTemplate
+							.replace(
+									"NOMBRE",
+									e.getFirstName())
+							.replace(
+									"MONTO",
+									String.valueOf(
+											e.getAmount()))
+							.replace(
+									"OBSERVACION",
+									"Este mensaje duró en llegar: " + (e.getTerm()) + " Sec");
 
-		// 			whatsApp
-		// 					.setReceiverPhone(
-		// 							new PhoneNumber(
-		// 									"whatsapp:+505" + e.getPhoneNumber()));
+					whatsApp
+							.setReceiverPhone(
+									new PhoneNumber(
+											"whatsapp:+505" + e.getPhoneNumber()));
 
-		// 			whatsApp
-		// 					.setMessageBody(
-		// 							finalMessage);
+					whatsApp
+							.setMessageBody(
+									finalMessage);
 
-		// 			try {
-		// 				System.out.println(
-		// 						"Thread is sleeping... by " + (e.getTerm()) + " Sec");
+					try {
+						System.out.println(
+								"Thread is sleeping... by " + (e.getTerm()) + " Sec");
 
-		// 				Thread.sleep(Long.valueOf((1000 * e.getTerm())));
+						Thread.sleep(Long.valueOf((1000 * e.getTerm())));
 
-		// 			} catch (InterruptedException e1) {
-		// 				e1.printStackTrace();
-		// 			}
-		// 			whatsApp
-		// 					.send();
-		// 			System.out.println(finalMessage);
-		// 		});
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+					whatsApp
+							.send();
+					System.out.println(finalMessage);
+				});
 
 		whatsApp.setReceiverPhone(
 				new PhoneNumber(

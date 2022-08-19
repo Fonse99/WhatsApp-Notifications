489b2d4aa8b5180301426e0c02be1b0a0d64c360
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2022-08-18 21:10:20 -0600

    Fix: moving to right package

---
+++ src/main/java/com/nuvissoft/notifications/NotificationsApplication.java
@@ -0,0 +1,86 @@
+package com.nuvissoft.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.nuvissoft.notifications.Services.CreditServices;
+import com.nuvissoft.notifications.Services.WhatsAppSender;
+import com.twilio.type.PhoneNumber;
+
+import Utilities.Environment;
+
+@SpringBootApplication
+public class NotificationsApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		SpringApplication.run(NotificationsApplication.class, args);
+		WhatsAppSender whatsApp = new WhatsAppSender();
+		demoWhatsAppMessageWithExcel(whatsApp);
+
+		
+	}
+
+	static void demoWhatsAppMessageWithExcel(WhatsAppSender whatsApp) {
+		
+		CreditServices service = new CreditServices();
+		String messageTemplate = Environment.env.get("TWILIO_CUSTOM_MESSAGE");
+		String paymentReportMessage = Environment.env.get("PAYMENT_REPORT_MESSAGE");
+		var notificationsSended = service.getAllCreditsWithLate();
+
+		notificationsSended
+				.forEach(e -> {
+
+					String finalMessage = messageTemplate
+							.replace(
+									"NOMBRE",
+									e.getFirstName())
+							.replace(
+									"MONTO",
+									String.valueOf(
+											e.getAmount()))
+							.replace(
+									"OBSERVACION",
+									"Este mensaje duró en llegar: " + (e.getTerm()) + " Sec");
+
+					whatsApp
+							.setReceiverPhone(
+									new PhoneNumber(
+											"whatsapp:+505" + e.getPhoneNumber()));
+
+					whatsApp
+							.setMessageBody(
+									finalMessage);
+
+					try {
+						System.out.println(
+								"Thread is sleeping... by " + (e.getTerm()) + " Sec");
+
+						Thread.sleep(Long.valueOf((1000 * e.getTerm())));
+
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+					whatsApp
+							.send();
+					System.out.println(finalMessage);
+				});
+
+		whatsApp.setReceiverPhone(
+				new PhoneNumber(
+						Environment.env.get("PHONE_TO_SEND_REPORT")));
+
+		paymentReportMessage = paymentReportMessage
+				.replace(
+						"COBROS",
+						notificationsSended.toString().replaceAll("[\\[\\]]", ""));
+
+		whatsApp.setMessageBody(
+				paymentReportMessage);
+
+		System.out.println(paymentReportMessage);
+
+		whatsApp.send();
+
+	}
+}
---
+++ src/main/java/com/nuvissoft/notifications/Services/CreditServices.java
@@ -0,0 +1,83 @@
+package com.nuvissoft.notifications.Services;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import Data.Domain.Credits;
+import Data.Repositories.ExcelRepository;
+import Utilities.Environment;
+
+public class CreditServices implements ExcelRepository<Credits> {
+
+    String filePath = Environment.env.get("XLSX_PATH_FILE");
+    FileInputStream excelFile;
+    List<Credits> allCredits;
+
+    XSSFWorkbook creditBook;
+    XSSFSheet sheet;
+
+    @Override
+    public List<Credits> getAllCreditsWithLate() {
+
+        this.allCredits = new ArrayList<Credits>();
+
+        try {
+
+            this.excelFile = new FileInputStream(this.filePath);
+            this.creditBook = new XSSFWorkbook(excelFile);
+            this.sheet = this.creditBook.getSheetAt(0);
+            XSSFRow xssfRow;
+
+            for (int rowIndex = 1; rowIndex <= this.sheet.getLastRowNum(); rowIndex++) {
+
+                xssfRow = this.sheet.getRow(rowIndex);
+                this.allCredits.add(this.fillCreditObject(xssfRow));
+
+            }
+
+        } catch (IOException notFounded) {
+
+            System.err.println(notFounded.getLocalizedMessage());
+
+            notFounded.printStackTrace();
+
+        }
+
+        return this.allCredits;
+    }
+
+    private Credits fillCreditObject(XSSFRow row) {
+
+        XSSFCell cellPhoneNumber = row.getCell(7);
+
+        cellPhoneNumber.setCellType(CellType.STRING);
+        System.out.println(row.getCell(9).getCellType().name());
+
+        Credits credit;
+
+        credit = new Credits(
+                row.getCell(0).getStringCellValue(),
+                row.getCell(1).getStringCellValue(),
+                row.getCell(2).getStringCellValue(),
+                row.getCell(3).getStringCellValue(),
+                row.getCell(4).getStringCellValue(),
+                row.getCell(5).getStringCellValue(),
+                row.getCell(6).getStringCellValue(),
+                cellPhoneNumber.getStringCellValue(),
+                Integer.parseInt(
+                        row.getCell(8).getRawValue()),
+                Double.parseDouble(
+                        row.getCell(9).getRawValue()));
+
+        return credit;
+
+    }
+
+}

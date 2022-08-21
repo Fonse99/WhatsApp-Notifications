15ef863c712c0a7c768b81e4d42eca738d2250c6
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2022-08-20 23:57:30 -0600

    Production config

--- src/main/java/com/nuvissoft/notifications/Utilities/Environment.java
+++ src/main/java/com/nuvissoft/notifications/Utilities/Environment.java
@@ -1,7 +1,10 @@
 package com.nuvissoft.notifications.Utilities;
 
+import java.util.Map;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Environment {
-    public static final Dotenv env = Dotenv.load();
+    // public static final Dotenv env = Dotenv.load();
+    public static final Map<String, String> env = System.getenv();
 }
--- src/main/java/com/nuvissoft/notifications/config/SpringBootConfig.java
+++ src/main/java/com/nuvissoft/notifications/config/SpringBootConfig.java
@@ -1,9 +1,11 @@
 package com.nuvissoft.notifications.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ComponentScan("com.nuvissoft.notifications")
 public class SpringBootConfig {
 }
